333bce8158993a03a3dde6d3bdf0109b0532ce51
cuishichao <csc@ihotel.cn>
2019-12-19 15:39:32 +0800

    测试 demo

---
+++ demo/src/main/java/demo/Test.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class Test {
+	@ResponseBody
+	@RequestMapping("/test")
+	public String copyProductInfo() {
+		return "123";
+	}
+}
